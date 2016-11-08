afc27c5ab2d6411330d90a15ecb95d439561d0c0
Bryan Horna <bryanjhv@gmail.com>
2016-11-08 19:54:32 +0000

    Move templating things out

--- index.js
+++ index.js
@@ -1,56 +1,55 @@
-/**
- * REQUIRES
- */
+/** REQUIRES **/
 
-var fs        = require('fs'),
-    marked    = require('marked'),
-    githubCss = require.resolve('github-markdown-css');
+var fs       = require('fs'),
+    path     = require('path'),
+    markdown = require('marked'),
+    cssPath  = require.resolve('github-markdown-css');
 
 
-/**
- * CONFIGURATION
- */
+/** CONFIGURATION **/
 
-var template = __dirname + '/readme.html';
+var tplPath = path.join(__dirname, 'template.html'),
+    tplText, cssText;
 
-var defaultOptions = {
-  routes: '/',
+var defaultOpts = {
+  routes:   '/',
   filename: 'README.md'
 };
 
 
-/**
- * EXPORTS
- */
+/** EXPORTS **/
 
 module.exports = expressReadme;
 
-function expressReadme(app, options) {
+
+/** CORE **/
+
+function expressReadme(app, opts) {
   // Fetch options
-  if (!options) options = {};
-  var routes = options.routes || defaultOptions.routes;
-  var filename = options.filename || defaultOptions.filename;
+  if (!opts) opts = {};
+  var routes   = opts.routes || defaultOpts.routes;
+  var filename = opts.filename || defaultOpts.filename;
   if (!Array.isArray(routes)) routes = [routes];
 
-  // Read things
-  var html = fs.readFileSync(template, 'utf8');
-  var css  = fs.readFileSync(githubCss, 'utf8');
-
-  // Replace things
-  html = html.replace('{{ filename }}', filename);
-  html = html.replace('{{ githubCss }}', css);
-
-  // Setup routes
   routes.forEach(function (route) {
     app.get(route, function (req, res) {
-      // Markdown -> HTML
-      var content = marked(fs.readFileSync(filename, 'utf8'));
+      var html = tplText.substr(0);
+      html = html.replace('{{filename}}', path.basename(filename));
 
-      // Replace content
-      html = html.replace('{{ content }}', content);
+      var exists = fs.existsSync(filename);
+      if (!exists) return res.sendStatus(404);
 
-      // Serve
+      var content = markdown(fs.readFileSync(filename, 'utf8'));
+      html = html.replace('{{content}}', content);
       res.status(200).type('html').end(html);
     });
   });
 }
+
+
+/** READING **/
+
+tplText = fs.readFileSync(tplPath, 'utf8');
+cssText = fs.readFileSync(cssPath, 'utf8');
+
+tplText = tplText.replace('{{styles}}', cssText);
